ff29444b528bf6b444f002dacc1069095e571bad
Max <max.pihlstrom@gmail.com>
2014-02-03 13:02:29 +0100

    files

---
+++ src/todo/Todo.java
@@ -0,0 +1,34 @@
+package todo;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Todo {
+
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        JFrame frame = new JFrame("FrameDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        JLabel emptyLabel = new JLabel("");
+        emptyLabel.setPreferredSize(new Dimension(175, 100));
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+ 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+    
+    
+    public static void main(String[] args) {
+        //Schedule a job for the event-dispatching thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+    
+}
