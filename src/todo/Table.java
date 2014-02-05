9d83f34651e370df1c0bf07f8bc784038e48bb09
tonybj <tony_bjorkman@hotmail.com>
2014-02-05 17:18:49 +0100

    right click submenu on table
    Rightclick selects a row and opens a submenu with option to remove
    event.

--- src/todo/Table.java
+++ src/todo/Table.java
@@ -2,11 +2,15 @@ package todo;
 
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
@@ -19,10 +23,15 @@ import javax.swing.table.DefaultTableModel;
 
 public class Table extends JPanel {
     private boolean DEBUG = false;
+    private JPopupMenu popMenu;
+    private Table myTable;
+    final JTable table;
     
     public Table() {
         super(new GridLayout(1,0));
 
+        myTable = this;
+        
         String[] columnNames = {"Description",
                                 "Due date",
                                 "Category",
@@ -34,10 +43,28 @@ public class Table extends JPanel {
             {"Exam studies", "2014-02-06", "School", new Integer(2)},
             {"Water the plants", "2014-02-07", "Home", new Integer(3)}
         };
+        
+        JMenuItem menuItemAdd = new JMenuItem("Add event");
+        menuItemAdd.addActionListener(new ActionListener(){
+        	public void actionPerformed(ActionEvent e){
+        		int r=table.getSelectedRow();
+        		System.out.println(""+r);
+				ImmutableTableModel dataModel = (ImmutableTableModel) table.getModel();
+				
+        		dataModel.removeRow(r);
+        	}
+
+        });
+        
+        
+        popMenu = new JPopupMenu();
+        
+        popMenu.add(menuItemAdd);
+        
 
         ImmutableTableModel model = new ImmutableTableModel(data, columnNames);
         model.setColumnImmutable(2, true);
-        final JTable table = new JTable(model);
+        table = new JTable(model);
         table.setPreferredScrollableViewportSize(new Dimension(500, 70));
         table.setFillsViewportHeight(true);
         table.setAutoCreateRowSorter(true);
@@ -47,15 +74,24 @@ public class Table extends JPanel {
         table.addMouseListener(new MouseAdapter(){
 
 			@Override
-			public void mouseClicked(MouseEvent arg0) {
+			public void mouseReleased(MouseEvent arg0) {
 				
 				if(arg0.getButton()==arg0.BUTTON3){
-				// TODO Auto-generated method stub
-				JTable tableS = (JTable) arg0.getSource();
-				ImmutableTableModel dataModel = (ImmutableTableModel) tableS.getModel();
+				JTable clickedTable = (JTable) arg0.getSource();
+				int r = clickedTable.rowAtPoint(arg0.getPoint());
+				if(r>=0 && r < clickedTable.getRowCount()){
+				clickedTable.setRowSelectionInterval(r, r);
+					// TODO Auto-generated method stub
+				popMenu.show(arg0.getComponent(),arg0.getX(),arg0.getY());
+				}
+				else
+				{
+					clickedTable.clearSelection();
+				}
+				/*JTable tableS = (JTable) arg0.getSource();
 				ListSelectionModel LSmodel = tableS.getSelectionModel();
 				System.out.println(""+ LSmodel.getMinSelectionIndex());
-				dataModel.removeRow(LSmodel.getMinSelectionIndex());
+				dataModel.removeRow(LSmodel.getMinSelectionIndex());*/
 				}
 			}
 
