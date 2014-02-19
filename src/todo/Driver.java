4df6c95785e81f20b15ba87babfabeb0605e7fa3
Max <max.pihlstrom@gmail.com>
2014-02-19 22:34:31 +0100

    commiting everything

---
+++ src/todo/Driver.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package todo;
+
+import view.TodoWindow;
+
+/**
+ *
+ * @author Jarl
+ */
+
+public class Driver {
+	
+    public Driver() {
+        
+    }
+       
+    
+    public static void main(String[] args) {
+
+        TodoWindow todo = new TodoWindow();
+    }
+}
